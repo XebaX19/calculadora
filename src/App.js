e01f9b626b06b8f58020b1967dd62c51ee36e3fd
XebaX19 <seba.bellesi@gmail.com>
2022-06-20 00:11:01 -0300

    Pruebas ReacJS - Calculadora

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './imagenes/freeCodeCamp-logo.png';
+import Boton from './componentes/Boton.jsx';
+import Pantalla from './componentes/Pantalla.jsx';
+import BotonClear from './componentes/BotonClear.jsx';
+import { useState } from 'react';
+import { evaluate } from 'mathjs'; //Librería instalada desde NPM
 
 function App() {
+
+  let [input, setInput] = useState('');
+
+  const esOperador = (valor) => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
+  };
+
+  const agregarInput = (valor) => {
+    
+    //Si es el 1er ingreso, verifico que sea un caracter de inicio válido
+    if (input === '') {
+      if (['+', '*', '/', '.'].includes(valor)) {
+        return;
+      }
+    }
+    
+    //Si ingreso un "." y el anterior ingreso fue un ".", no lo permito
+    if (valor === '.') {
+      if (input) {
+        let inputTexto = input.toString();
+
+        if (inputTexto.charAt(inputTexto.length-1) === '.') {
+          return;
+        }
+      }
+    }
+
+    //Si ingreso un "." y el anterior ingreso fue un operador, no lo permito
+    if (valor === '.') {
+      if (input) {
+        let inputTexto = input.toString();
+
+        if (esOperador(inputTexto.charAt(inputTexto.length-1))) {
+          return;
+        }
+      }
+    }
+
+    //Si ingreso un operador y el anterior ingreso fue un ".", no lo permito
+    if (esOperador(valor)) {
+      if (input) {
+        let inputTexto = input.toString();
+
+        if (inputTexto.charAt(inputTexto.length-1) === '.') {
+          return;
+        }
+      }
+    }
+
+    //Si ingreso un operador y el anterior ingreso fue otro operador, lo reemplazo
+    if (esOperador(valor)) {
+      if (input) {
+        let inputTexto = input.toString();
+
+        if (esOperador(inputTexto.charAt(inputTexto.length-1))) {
+          setInput(inputTexto.substring(0, inputTexto.length-1)+valor);
+          return;
+        }
+      }
+    }
+    
+    setInput(input + valor);
+  };
+
+  const calcularResultado = () => {
+    if (input) { //Evalúo sólo si el input tiene contenido
+      let inputTexto = input.toString();
+      if (esOperador(inputTexto.charAt(inputTexto.length-1))) {
+        setInput(evaluate(inputTexto.substring(0, inputTexto.length-1)));
+      } else {
+        setInput(evaluate(inputTexto)); //evaluate de Mathjs, evalúa la cadena de caracteres y en caso de que sea una expresión matemática válida, calcula y devuelve el resultado
+      }  
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={ freeCodeCampLogo }
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp' /> 
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla 
+          input={ input } />
+        <div className='fila'>
+          <Boton manejarClic={ agregarInput }>1</Boton>
+          <Boton manejarClic={ agregarInput }>2</Boton>
+          <Boton manejarClic={ agregarInput }>3</Boton>
+          <Boton manejarClic={ agregarInput }>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={ agregarInput }>4</Boton>
+          <Boton manejarClic={ agregarInput }>5</Boton>
+          <Boton manejarClic={ agregarInput }>6</Boton>
+          <Boton manejarClic={ agregarInput }>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={ agregarInput }>7</Boton>
+          <Boton manejarClic={ agregarInput }>8</Boton>
+          <Boton manejarClic={ agregarInput }>9</Boton>
+          <Boton manejarClic={ agregarInput }>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={ calcularResultado }>=</Boton>
+          <Boton manejarClic={ agregarInput }>0</Boton>
+          <Boton manejarClic={ agregarInput }>.</Boton>
+          <Boton manejarClic={ agregarInput }>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear manejarClear={ () => setInput('') }>
+            Clear
+          </BotonClear>
+        </div>
+      </div>
     </div>
   );
 }
